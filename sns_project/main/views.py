346ec8614f438c27807d3eda5001077809501dc1
yaena1223 <rewrite_the_stars_@naver.com>
2021-04-27 17:24:15 +0900

    mypage

---
+++ sns_project/main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def showmain(request):
+    return render(request, 'main/mainpage.html')
+
+def show(request):
+    return render(request,'main/show.html')
+
+
